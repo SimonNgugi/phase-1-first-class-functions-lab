8a67dcb75ee0a2a319b31cb50a6bab9e26dd1cfc
Simon Ngugi Ngigi <simonngugi88@gmail.com>
2022-08-22 21:01:55 +0300

    Comleted assignment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Code your solution in this file!
+const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+const returnFirstTwoDrivers = function() {
+    return drivers.slice(0,2);
+}
+
+const returnLastTwoDrivers = function() {
+    return drivers.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+ const createFareMultiplier = function(fareMultiplier) {
+    return function(value) {
+        return fareMultiplier * value
+    }
+}
+
+const fareDoubler = createFareMultiplier(2);
+const  fareTripler = createFareMultiplier(3);
+
+function selectDifferentDrivers (arrayOfDrivers, returnFirstTwoDrivers) {
+    return returnFirstTwoDrivers (arrayOfDrivers)
+}
+
+function selectDifferentDrivers(arrayOfDrivers,returnLastTwoDrivers){
+    return returnLastTwoDrivers(arrayOfDrivers)
+}
